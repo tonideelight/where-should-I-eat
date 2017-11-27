d226b4fa02c3ba9487dafac51296d3ec4e8a943a
Toni <deea@seattleu.edu>
2017-11-26 20:10:41 -0800

    some updates

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<h1>Sunrise/Sunset Service</h1>
+<h2>City Search</h2>
+<form v-on:submit.prevent="getCities">
+        <p>Enter city name: <input type="text" v-model="query" placeholder="Seattle"> <button type="submit">Go</button></p>
+    </form>
+</div>    
+</template>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
