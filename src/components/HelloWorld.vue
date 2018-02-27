a719138a1b7dc162220f721ba132a0890bc2d31a
Toni <deea@seattleu.edu>
2018-02-26 20:34:01 -0800

    add embedURL and get restaurant function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <form v-on:submit.prevent="findAddress">
       <p>Find restuarants for this address:<input type="text" v-model="address"><button type="submit">Search</button></p>
     </form>
-    
+        <iframe v-if="embedURL" v-bind:src="embedURL"></iframe>
       <router-link :to="{ name: 'Weekly', params: { lat: this.lat, lng: this.lng }}">Click here to find restaurant</router-link>
 </div>    
 </template>
@@ -18,7 +18,8 @@ export default {
       errors: [],
       address: '',
       lat: '',
-      lng: ''
+      lng: '',
+      embedURL: ''
     }
   },
   methods: {
@@ -33,10 +34,25 @@ export default {
         this.results = response.data.results;
         this.lat = this.results[0].geometry.location.lat;
         this.lng = this.results[0].geometry.location.lng;
+        this.findNearbyRestaurant();
       })
       .catch(error => {
         this.errors.push(error);
       });
+    },
+    findNearbyRestaurant: function() {
+      axios.get('https://maps.googleapis.com/maps/api/place/radarsearch/json', {
+        params: {
+          key: 'AIzaSyCaENM1qL1ZLAuVH4zczvi-_G2LPaaMA_Q',
+          location: `${this.lat},${this.lng}`,
+          radius: 5000,
+          type: 'restaurant'
+        }
+      })
+      .then(response => {
+        const place_id = response.results[0]["place_id"];
+        this.embedURL = `https://www.google.com/maps/embed/v1/view?key=AIzaSyDpfr0LbX7DxQNVzUsxAObNkeMQKC1SiFU&origin=place_id:${place_id}`;
+      })
     }
   }
 }
--- src/components/Weekly.vue
+++ src/components/Weekly.vue
@@ -15,7 +15,7 @@ export default {
   data () {
     return {
       results: null,
-      errors: [],
+      errors: [], 
       lat: '',
       lng: ''
     }
